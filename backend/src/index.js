90f8f03a9cb49235fc90c8ceea7056f80b230fb7
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-08-03 17:07:30 +0530

    backend bug2

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,13 +29,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(pathjoin(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
+
 
 server.listen(3000, () => {
   console.log("server running on " + PORT);
   ConnectDB();
-
 
 })
