bddcbfc561df8eb5904c0dcd73f327219c232c7d
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-08-03 17:26:57 +0530

    change the axios url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
+// lib/axios.js
 import axios from "axios";
 
+const base_url = import.meta.env.VITE_API_URL;
+
 export const axiosInstance = axios.create({
-  baseURL:import.meta.env.MODE ==="development"?"http://localhost:3000/api":"/api",
-  withCredentials:true
+  baseURL: base_url,
+  withCredentials: true,
 });
