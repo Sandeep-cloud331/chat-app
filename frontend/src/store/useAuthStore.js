f36d623747a4577f3d343385c56b19e25bf4c229
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-08-03 19:57:41 +0530

    req bug

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const base_url = "https://chat-app-1-6heo.onrender.com";
+const base_url = "https://chat-app-1-6heo.onrender.com/api";
 
 export const useAuthStore = create((set, get) => ({
   onlineUsers: [],
