a7347070b04da1e7686dc05b2103915b361e7f39
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-08-03 20:10:24 +0530

    url fixes

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,6 +7,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "https://chat-app-aqbx.vercel.app",
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
