48692d74b5b8b9cbfe0f7fdb25662bb6d2de435f
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-08-03 20:03:35 +0530

    fix the cors bug

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,39 +1,37 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server,{
-  cors:{
-    origin:["http://localhost:5173"],
-    
-
+const io = new Server(server, {
+  cors: {
+    origin: "https://chat-app-aqbx.vercel.app",
   },
 });
 
-export function getRecieverSocketId(userId){
+export function getRecieverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-const userSocketMap={};
+const userSocketMap = {};
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
   const userId = socket.handshake.query.userId;
   console.log(userId);
-  
-  if(userId) userSocketMap[userId]= socket.id;
 
-  io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  if (userId) userSocketMap[userId] = socket.id;
 
-  socket.on("disconnect",()=>{
-    console.log("A user disconnected",socket.id);
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
   });
-  
+
 });
 
-export {io,app,server};
+export { io, app, server };
