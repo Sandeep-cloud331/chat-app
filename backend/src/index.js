3d9185adec9511b886406a9edb6440221362b1ff
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-08-03 17:09:49 +0530

    cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://chat-app-aqbx.vercel.app",
   credentials: true
 }));
 
